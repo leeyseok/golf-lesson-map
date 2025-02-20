5b84e0d99189d476cf2c4dd90491224aaa92ee34
leeyseok <eymin123@gmail.com>
2025-02-20 18:15:17 +0900

    name plus

--- components/ui/templates/Map.tsx
+++ components/ui/templates/Map.tsx
@@ -4,6 +4,7 @@ import { Loader } from '@googlemaps/js-api-loader';
 
 const MapComponent = () => {
   const mapRef = useRef<HTMLDivElement | null>(null);
+  const markerRef = useRef<google.maps.Marker[]>([]);
 
   useEffect(() => {
     const initMap = async () => {
@@ -11,26 +12,46 @@ const MapComponent = () => {
         apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY!,
         version: 'weekly',
       });
-
-      await loader.load();
-      
+      const { Map } = await loader.importLibrary('maps');
+      const { Marker } = await loader.importLibrary('marker') as google.maps.MarkerLibrary;
       if (!mapRef.current || !window.google) return;
-
+  
       const position = { lat: 35.681236, lng: 139.767125 };
-
+  
       const mapOptions: google.maps.MapOptions = {
         center: position,
         zoom: 14,
         mapId: 'MY_MAP_ID',
+        streetViewControl: false,
+        fullscreenControl: false,
+        mapTypeControl: false,
       };
-
-      const map = new window.google.maps.Map(mapRef.current, mapOptions);
-      console.log(map)
+  
+      const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
+  
+      // const markers: google.maps.Marker[] = [];
+      Array(10).fill(0).forEach((_, i) => {
+        const marker = new Marker({
+          map: map,
+          position: {
+            lat: position.lat + (0.002 * i),
+            lng: position.lng + (0.002 * i),
+          },
+          icon: {
+            url: "/marker2.png",
+            labelOrigin: new window.google.maps.Point(16, 16),
+          },
+          label: {
+            text: `ゴルフガイドゴルフガイド ${i + 1}`,
+            className: "marker-label",
+          },
+        });
+        markerRef.current.push(marker);
+      });
     };
-
+  
     initMap();
   }, []);
-
   return <div ref={mapRef} style={{ width: '100%', height: '100vh' }} />;
 };
 
